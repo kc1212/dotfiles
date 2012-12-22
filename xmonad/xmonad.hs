4696f4a161cba4e7f5b8d2a92f9fae018d047fba
kc <kelongcong@gmail.com>
2012-12-22 23:38:38 +0000

    updated submodules and added xmobarrc, fehbg

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,19 +1,25 @@
---
--- An example, simple ~/.xmonad/xmonad.hs file.
--- It overrides a few basic settings, reusing all the other defaults.
---
-
+-- Imports.
 import XMonad
+import XMonad.Hooks.DynamicLog
 
-main = do
-  xmonad $ defaultConfig
-    { terminal    = myTerminal
-    , modMask     = myModMask
-    , borderWidth = myBorderWidth
-    }
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
--- yes, these are functions; just very simple ones
--- that accept no input and return static values
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
+myConfig = defaultConfig { 
+  terminal = myTerminal, 
+  modMask = myModMask, 
+  borderWidth = myBorderWidth 
+}
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
