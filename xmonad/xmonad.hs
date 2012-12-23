fea884e93343bb3af84276c36e9b0f7710a0d0ce
kc <kelongcong@gmail.com>
2012-12-23 14:10:45 +0000

    added a lot of stuff

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,26 +1,26 @@
--- Imports.
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.Gaps
+import System.IO
 
--- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
--- Command to launch the bar.
-myBar = "xmobar"
-
--- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
-
--- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
--- Main configuration, override the defaults to your liking.
-myConfig = defaultConfig { 
-  terminal = myTerminal, 
-  modMask = myModMask, 
-  borderWidth = myBorderWidth 
-}
-
-myTerminal    = "urxvt"
-myModMask     = mod4Mask -- Win key or Super_L
-myBorderWidth = 3
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        -- , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = gaps [(U,17)] $ Tall 1 (3/100) (1/2) ||| Full
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , terminal = "terminator" -- change default terminal
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
