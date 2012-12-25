5c80b36e600ed0b6dc9d786416436c837967ce40
Kelong Cong <kelongcong@gmail.com>
2012-12-25 18:38:26 +0000

    some more adjustments

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,23 +3,35 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Layout.Gaps
+-- import XMonad.Layout.Gaps
 import System.IO
 
+
+myTerminal = "terminator"
+myBorderColor = "#00cc00"
+myBorderWidth = 2
+myModMask = mod4Mask -- Rebind Mod to the Windows key
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
+        {
+        layoutHook=avoidStruts $ layoutHook defaultConfig
+        , manageHook=manageHook defaultConfig <+> manageDocks
+        -- manageHook = manageDocks <+> manageHook defaultConfig
         -- , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , layoutHook = gaps [(U,17)] $ Tall 1 (3/100) (1/2) ||| Full
+        -- , layoutHook = gaps [(U,17)] $ Tall 1 (3/100) (1/2) ||| Full
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "terminator" -- change default terminal
-        } `additionalKeys`
+        , modMask = myModMask
+        , terminal = myTerminal
+        , focusedBorderColor = myBorderColor
+        , borderWidth = myBorderWidth
+        }
+
+        `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
--- zshrc
+++ zshrc
@@ -1,6 +1,6 @@
 # nothing here
 
-alias pd="pushd"
+alias pd="pushd" #use this with popd
 # alias open xdg-open
 # alias sicstus "rlwrap sicstus"
 alias xterm="xterm -bg black -fg white"
