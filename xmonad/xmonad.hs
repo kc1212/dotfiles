856ff6c73a66e0c42d4baa4396816b7fed047b3f
Kelong Cong <kelongcong@gmail.com>
2012-12-28 14:07:04 +0000

    improved xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,26 +1,26 @@
+ -- referenced from 
+ -- https://github.com/andrusha/dotfiles/blob/master/.xmonad/xmonad.hs
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Layout.Gaps -- try to not use this
+-- import XMonad.Layout.NoBorders
+-- import XMonad.Layout.Gaps -- try to not use this
 import System.IO
-
-
-myTerminal = "terminator"
-myBorderColor = "#00cc00"
-myBorderWidth = 2
-myModMask = mod4Mask -- Rebind Mod to the Windows key
 
 main = do
     xmproc <- spawnPipe "xmobar"
+    -- spawn myTrayer
+    -- spawn myDropbox
     xmonad $ defaultConfig
         {
-        -- layoutHook=avoidStruts $ layoutHook defaultConfig
-        -- , manageHook=manageHook defaultConfig <+> manageDocks
-        manageHook = manageDocks <+> manageHook defaultConfig
+        layoutHook=avoidStruts $ layoutHook defaultConfig
+        , manageHook=manageHook defaultConfig <+> manageDocks
+        -- manageHook = manageDocks <+> manageHook defaultConfig
         -- , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , layoutHook = gaps [(U,17)] $ Tall 1 (3/100) (1/2) ||| Full
+        -- , layoutHook = gaps [(U,17)] $ Tall 1 (3/100) (1/2) ||| Full
+        -- , layoutHook = smartBorders $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
@@ -29,6 +29,7 @@ main = do
         , terminal = myTerminal
         , focusedBorderColor = myBorderColor
         , borderWidth = myBorderWidth
+        , workspaces = myWorkspaces
         }
 
         `additionalKeys`
@@ -37,3 +38,15 @@ main = do
         , ((0, xK_Print), spawn "scrot")
         ]
 
+
+myTerminal = "terminator"
+myBorderColor = "#00cc00"
+myBorderWidth = 1
+myModMask = mod4Mask -- Rebind Mod to the Windows key
+
+-- Spawned processes
+-- myTrayer = "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 12 "
+-- myDropbox = "if [ ! \"$(pidof dropbox)\" ] ; then dropboxd ; fi"
+
+-- Workspaces
+myWorkspaces = ["main", "code", "web", "chat"] ++ map show [5..9]
