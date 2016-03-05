f2a0f24cc6c165909586e5669a634b0e08fc5e45
kc <kc04bc@gmx.com>
2016-03-05 23:32:09 +0100

    My nixconfig is weird - fixed, removed unused shell environment vars

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,16 +1,19 @@
 # install this environment using:
 # nix-env -f "<nixpkgs>" -iA myHaskellEnv
 
-{
-  packageOverrides = super: let self = super.pkgs; in
-  {
-    myHaskellEnv = self.haskellPackages.ghcWithHoogle
-                     (haskellPackages: with haskellPackages; [
-                       # libraries
-                       mtl containers bytestring directory filepath
-                       # tools
-                       cabal-install cabal2nix codex ghc-mod hasktags
-                     ]);
-  };
-}
+# {
+#   packageOverrides = super: let self = super.pkgs; in
+#   {
+#     myHaskellEnv = self.haskellPackages.ghcWithHoogle
+#                      (haskellPackages: with haskellPackages; [
+#                        # libraries
+#                        mtl containers bytestring directory filepath
+#                        # tools
+#                        stack cabal-install cabal2nix codex ghc-mod hasktags
+#                      ]);
+#   };
+# }
+#
+
+{ allowUnfree = true; }
 
