67d850dc9e22672e9b9c19eaee62da1980128b6e
kc1212 <kc1212@users.noreply.github.com>
2016-04-27 16:25:22 +0200

    Fish files

---
+++ config.fish
@@ -0,0 +1,27 @@
+## Environment variables and PATH
+# general
+set -x EDITOR nvim
+
+## nix
+source $HOME/.config/fish/nix.fish
+
+# rust
+set -x RUST_SRC_PATH $HOME/rustcode/rust/src
+set PATH $PATH $HOME/.cargo/bin
+
+# golang
+set -x GOPATH $HOME/gocode
+set PATH $PATH $GOPATH/bin
+
+# stack (local bin)
+set PATH $PATH $HOME/.local/bin
+
+
+## Aliases
+alias cp 'cp -i'
+alias mv 'mv -i'
+alias ln 'ln -i'
+alias chown 'chown --preserve-root'
+alias chmod 'chmod --preserve-root'
+alias chgrp 'chgrp --preserve-root'
+alias tmux 'tmux -2'
